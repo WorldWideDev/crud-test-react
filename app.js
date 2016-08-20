394b774122b14c52ff47256a78430f88f1a7e349
WorldWideDev <dev206@gmail.com>
2016-08-20 00:03:42 -0700

    added compoent and react-dom render method

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log(React)
+var MyComponent = React.createClass({
+    render: function(){
+        return <p>YAY</p>
+    }
+})
+ReactDOM.render(
+    <MyComponent/>,
+    document.getElementById('app')
+)
